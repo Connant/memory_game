93559c04c9021470dacf96a45556ffc58fcda7c7
Дарья 🥝 <daria@MacBook-Pro-Dara.local>
2022-10-14 15:40:12 +0700

    game__first

---
+++ src/cards.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import { main, images } from "./date";
+
+export default function MemoryGame() {
+  const [imagesArray, setImagesArray] = useState([]);
+  const [chosen, setChosen] = useState([]);
+  const [chosenIds, setChosenIds] = useState([]);
+  const [points, setPoints] = useState(0);
+
+  const [openCards, setOpenCards] = useState([]);
+
+  const createCardBoard = () => {
+    const imagesGenerated = images?.concat(...images);
+    const shuffledArray = shuffleArray(imagesGenerated);
+    setImagesArray(shuffledArray);
+  };
+
+  const flipImage = (image, index) => {
+    if (chosenIds?.length === 1 && chosenIds[0] === index) {
+      return;
+    }
+
+    if (chosen?.length < 2) {
+      setChosen((chosen) => chosen?.concat(image));
+      setChosenIds((chosenIds) => chosenIds?.concat(index));
+
+      if (chosen?.length === 1) {
+        if (chosen[0] === image) {
+          setPoints((points) => points + 2);
+          setOpenCards((openCards) => openCards?.concat([chosen[0], image]));
+        }
+        setTimeout(() => {
+          setChosenIds([]);
+          setChosen([]);
+        }, 700);
+      }
+    }
+  };
+
+  const isCardChosen = (image, index) => {
+    return chosenIds?.includes(index) || openCards?.includes(image);
+  };
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const startOver = () => {
+    setChosenIds([]);
+    setChosen([]);
+    setPoints(0);
+    setOpenCards([]);
+  };
+
+  useEffect(() => {
+    createCardBoard();
+  }, []);
+
+  return (
+    <section className="content">
+      <h2 className="content__title">Memory Game</h2>
+      <h3 className="content__count">Points: {points}</h3>
+      <button className="content__button" onClick={startOver}>
+        Start over
+      </button>
+      <div className="content__cards row no-gutters">
+        {imagesArray?.map((image, index) => {
+          return (
+            <div
+              className="col-4 col-lg-2"
+              key={index}
+              onClick={() => flipImage(image, index)}
+            >
+              <img
+                src={isCardChosen(image, index) ? image : main}
+                alt=""
+                className='content__cards--img'
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
---
+++ src/date.js
@@ -0,0 +1,46 @@
+import bg from "./imgs/0_main.svg";
+import one from "./imgs/1.svg";
+import two from "./imgs/2.svg";
+import three from "./imgs/3.svg";
+import four from "./imgs/4.svg";
+import five from "./imgs/5.svg";
+import six from "./imgs/6.svg";
+import seven from "./imgs/7.svg";
+import eight from "./imgs/8.svg";
+import nine from "./imgs/9.svg";
+import ten from "./imgs/10.svg";
+// import eleven from "./imgs/11.svg";
+// import twelve from "./imgs/12.svg";
+// import threeteen from "./imgs/13.svg";
+// import fourteen from "./imgs/14.svg";
+// import fifteen from "./imgs/15.svg";
+// import sixteen from "./imgs/16.svg";
+// import seventeen from "./imgs/17.svg";
+// import eighteen from "./imgs/18.svg";
+// import nineteen from "./imgs/19.svg";
+// import twenty from "./imgs/20.svg";
+
+export const main = bg;
+
+export const images = [
+	one,
+	two,
+	three,
+	four,
+	five,
+	six,
+	seven,
+	eight,
+	nine,
+	ten,
+	// eleven,
+	// twelve,
+	// threeteen,
+	// fourteen,
+	// fifteen,
+	// sixteen,
+	// seventeen,
+	// eighteen,
+	// nineteen,
+	// twenty,
+];
